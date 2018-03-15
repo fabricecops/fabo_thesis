131b64a47771ffea9ba50c1d91824b5c3244aa48
fabricecops <fabricecops@gmail.com>
2018-03-15 13:42:45 +0100

    add dockerfile

--- docker/run.sh
+++ docker/run.sh
@@ -1,6 +1,4 @@
-docker run -it --name  fabo_thesis \
-    -p 11345:11345 \
-    -p 11311:11311 \
+docker run -it --name  fabo-thesis \
     -p 8888:8888 \
     -v /home/fabo/fabo_thesis:/app \
     fabo-thesis
