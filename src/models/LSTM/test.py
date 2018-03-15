6fffc5f46689f90e70a0271f39469e16e6917ed6
fabricecops <fabricecops@gmail.com>
2018-03-15 13:20:44 +0100

    variance test finished

--- src/models/LSTM/test.py
+++ src/models/LSTM/test.py
@@ -2,6 +2,8 @@ from src.models.LSTM.configure import return_dict_bounds
 from src.models.LSTM.main_LSTM import model_mng
 from src.dst.outputhandler.pickle import tic,toc,pickle_save_
 import numpy as np
+import matplotlib.pyplot as plt
+
 class model_tests():
 
 
@@ -43,8 +45,8 @@ class model_tests():
         dict_['shuffle_random']['train_mean'] = np.mean(dict_['shuffle_random']['train'])
         dict_['shuffle_random']['train_std']  = np.std(dict_['shuffle_random']['train'])
 
-        dict_['shuffle_random']['val_mean'] = np.mean(dict_['shuffle_random']['val'])
-        dict_['shuffle_random']['val_std']  = np.std(dict_['shuffle_random']['val'])
+        dict_['shuffle_random']['val_mean']   = np.mean(dict_['shuffle_random']['val'])
+        dict_['shuffle_random']['val_std']    = np.std(dict_['shuffle_random']['val'])
 
         self.dict_c['path_save']     = './models/variance/shuffle_segmentated/'
         self.dict_c['shuffle_style'] = 'testing'
@@ -70,10 +72,40 @@ class model_tests():
 
 
 
-        path = './models/variance/variance.p'
-        pickle_save_(path,dict_)
+        path = './models/variance/'
+        pickle_save_(path+'variance.p',dict_)
         print(dict_)
+        fig = plt.figure(figsize=(16, 4))
 
+        ax1 = plt.subplot(131)
+        ax1.hist(dict_['shuffle_random']['train'],      label = 'random',color = 'g', alpha = 0.5, bins = 10)
+        ax1.hist(dict_['shuffle_segmentated']['train'], label = 'segmentated', color = 'r', alpha = 0.5, bins = 10)
+        plt.xlabel('AUC')
+        plt.ylabel('Frequency')
+        plt.legend()
+        plt.title('Train distribution')
+
+
+        ax2 = plt.subplot(132)
+        ax2.hist(dict_['shuffle_random']['val'],      label = 'random',color = 'g', alpha = 0.5, bins = 10)
+        ax2.hist(dict_['shuffle_segmentated']['val'], label = 'segmentated', color = 'r', alpha = 0.5, bins = 10)
+        plt.xlabel('AUC')
+        plt.ylabel('Frequency')
+        plt.legend()
+        plt.title('val distribution')
+
+        ax3 = plt.subplot(133)
+        ax3.hist(dict_['shuffle_random']['time'],      label = 'random',color = 'g', alpha = 0.5, bins = 10)
+        ax3.hist(dict_['shuffle_segmentated']['time'], label = 'segmentated', color = 'r', alpha = 0.5, bins = 10)
+        plt.xlabel('AUC')
+        plt.ylabel('Frequency')
+        plt.legend()
+        plt.title('Time distribution')
+
+
+        plt.savefig(path+'distribution.png')
+
+        plt.close('all')
 
 
 
